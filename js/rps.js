fa02cb57da195b55552f6d1cfdbbb96993aa823c
brandonkwong <brandonjk@gmail.com>
2014-07-15 01:16:35 -0700

    Updated HTML, CSS, JS

--- js/rps.js
+++ js/rps.js
@@ -1,115 +1,128 @@
 window.onload = function() {
 
-// CHANGE TO camelCase
+  // Global Variables
+  var playerHand, enemyHand;
+  var playerBox = document.getElementById('player-box');
+  var enemyBox = document.getElementById('enemy-box');
+
+  var player = document.getElementById('player');
+  var enemy = document.getElementById('enemy');
+  var countdown = document.getElementById('countdown');
+
+  var ex = [].slice.call(document.querySelectorAll('.lose'));
+  var lose = player.querySelector('.lose');
+  var win = enemy.querySelector('.lose');
+
+  var gameActive = false;
+  var battleActive = false;
+  var countActive = false;
 
 
-// Global Variables
-var playerHand, enemyHand;
-
-var player = document.getElementById('player');
-var enemy = document.getElementById('enemy');
-var countdown = document.getElementById('countdown');
-
-var ex = [].slice.call(document.querySelectorAll('.lose'));
-var lose = player.querySelector('.lose');
-var win = enemy.querySelector('.lose');
-
-var gameActive = false;
-var battleActive = false;
-
-
-// Function to randomize enemy hand
-function enemyRand() {
-  enemyHand = Math.floor(Math.random() * 3) + 1;
-  
-  if (enemyHand == 1) {
-    enemyHand = 'rock';
-    enemy.classList.add('rock');
-  }
-  else if (enemyHand == 2) {
-    enemyHand = 'paper';
-    enemy.classList.add('paper');
-  }
-  else {
-    enemyHand = 'scissors';
-    enemy.classList.add('scissors');
-  }
-
-  battleResults();
-}
-
-
-// Function for battle results
-function battleResults() {
-  if (playerHand == 'rock') {
-    if (enemyHand == 'rock') {
-      countdown.innerText = 'TIE';
-      tieBreaker();
-      return 'tie'
+  // Game Init
+  function gameInit() {
+    while (countActive == false) {
+      playerHand = undefined;
+      enemyHand = undefined;
+      player.removeAttribute('class');
+      enemy.removeAttribute('class');
+      playerBox.removeAttribute('class');
+      enemyBox.removeAttribute('class');
+      gameActive = true;
+      battleActive = false;
+      time = true;
+      ex.forEach(function(el) {
+        el.classList.remove('show', 'animated', 'bounceInDown');
+      });
+      count();
     }
-    else if (enemyHand == 'paper') {
-      lose.classList.add('show');
-      return 'lose'
+  }
+
+
+  // Function to Randomize Enemy Hand
+  function enemyRand() {
+    enemyHand = Math.floor(Math.random() * 3) + 1;
+    enemyBox.classList.add('animated', 'rotateInDownRight');
+    if (enemyHand == 1) {
+      enemyHand = 'rock';
+      enemy.classList.add('rock');
+    }
+    else if (enemyHand == 2) {
+      enemyHand = 'paper';
+      enemy.classList.add('paper');
     }
     else {
-      win.classList.add('show');
-      return 'win'
-    } 
+      enemyHand = 'scissors';
+      enemy.classList.add('scissors');
+    }
+    battleResults();
   }
 
-  else if (playerHand == 'paper') {
-    if (enemyHand == 'rock') {
-      win.classList.add('show');
-      return 'win'
+
+  // Function for Battle Results
+  function battleResults() {
+    if (playerHand == 'rock') {
+      if (enemyHand == 'rock') {
+        countdown.innerText = 'TIE';
+        tieBreaker();
+        return 'tie'
+      }
+      else if (enemyHand == 'paper') {
+        lose.classList.add('show', 'animated', 'bounceInDown');
+        return 'lose'
+      }
+      else {
+        win.classList.add('show', 'animated', 'bounceInDown');
+        return 'win'
+      } 
     }
-    else if (enemyHand == 'paper') {
-      countdown.innerText = 'TIE';
-      tieBreaker();
-      return 'tie'
+    else if (playerHand == 'paper') {
+      if (enemyHand == 'rock') {
+        win.classList.add('show', 'animated', 'bounceInDown');
+        return 'win'
+      }
+      else if (enemyHand == 'paper') {
+        countdown.innerText = 'TIE';
+        tieBreaker();
+        return 'tie'
+      }
+      else {
+        lose.classList.add('show', 'animated', 'bounceInDown');
+        return 'lose'
+      }
+    }
+    else if (playerHand == 'scissors') {
+      if (enemyHand == 'rock') {
+        lose.classList.add('show', 'animated', 'bounceInDown');
+        return 'lose'
+      }
+      else if (enemyHand == 'paper') {
+        win.classList.add('show', 'animated', 'bounceInDown');
+        return 'win'
+      }
+      else {
+        countdown.innerText = 'TIE';
+        tieBreaker();
+        return 'tie'
+      }
     }
     else {
-      lose.classList.add('show');
+      lose.classList.add('show', 'animated', 'bounceInDown');
+      countdown.innerText = 'TOO SLOW';
       return 'lose'
     }
   }
 
-  else if (playerHand == 'scissors') {
-    if (enemyHand == 'rock') {
-      lose.classList.add('show');
-      return 'lose'
-    }
-    else if (enemyHand == 'paper') {
-      win.classList.add('show');
-      return 'win'
-    }
-    else {
-      countdown.innerText = 'TIE';
-      tieBreaker();
-      return 'tie'
-    }
-  }
 
-  else {
-    lose.classList.add('show');
-    countdown.innerText = 'TOO SLOW';
-    return 'lose'
-  }
-}
-
-
-// Function for countdown
-var countActive = false;
-
-function count() {
-  countActive = true;
-
+  // Function for Countdown
+  function count() {
+    countActive = true;
     time = 3 + 1;
     var counter = setInterval(function() {
       time--
       if (time >= 0) {
         if (battleActive == true) {
           clearInterval(counter);
-          lose.classList.add('show');
+          lose.classList.add('show', 'animated', 'bounceInDown');
           countdown.innerText = 'TOO FAST';
           countActive = false;
         }
@@ -136,156 +149,138 @@ function count() {
         }
       }
     }, 1000);
+  }
 
-}
 
-
-// Player Controllers
-
-var handBtn = [].slice.call( document.querySelectorAll('.hand') );
-
-handBtn.forEach(function(btn) {
-  btn.addEventListener('click', function() {
-    while (gameActive == true && battleActive == false) {
-      if (time == 0) {
-        var hand = btn.getAttribute('hand');
-        playerHand = hand;
-        player.classList.add(hand);
-        battleActive = true;
-        enemyRand();
+  // Tie Breaker
+  function tieBreaker() {
+    var tieTime = 0;
+    var tieCounter = setInterval(function() {
+      tieTime++
+      if (tieTime > 0) {
+        clearInterval(tieCounter);
+        tieTime = 0;
+        gameInit();
       }
-      else {
-        var hand = btn.getAttribute('hand');
-        playerHand = hand;
-        player.classList.add(hand);
-        battleActive = true;
+    }, 1000);
+  }
+
+
+  // Player Controllers
+  var handBtn = [].slice.call(document.querySelectorAll('#rock, #paper, #scissors'));
+
+  handBtn.forEach(function(btn) {
+    btn.addEventListener('mousedown', function() {
+      btn.classList.add('touch');
+      while (gameActive == true && battleActive == false) {
+        playerBox.classList.add('animated', 'rotateInDownLeft');
+        if (time == 0) {
+          var hand = btn.getAttribute('hand');
+          playerHand = hand;
+          player.classList.add(hand);
+          battleActive = true;
+          enemyRand();
+        }
+        else {
+          var hand = btn.getAttribute('hand');
+          playerHand = hand;
+          player.classList.add(hand);
+          battleActive = true;
+        }
       }
+    });
+    btn.addEventListener('mouseup', function() {
+      btn.classList.remove('touch');
+    });
+  });
+
+  var rock = document.getElementById('rock');
+  var paper = document.getElementById('paper');
+  var scissors = document.getElementById('scissors');
+
+  document.addEventListener('keydown', function(e) {  
+    switch(e.keyCode) {
+      case 49: // Rock [ 1 ]
+        rock.classList.add('touch');
+        while (gameActive == true && battleActive == false) {
+          playerBox.classList.add('animated', 'rotateInDownLeft');
+          if (time == 0) {
+            playerHand = 'rock';
+            player.classList.add('rock');
+            battleActive = true;
+            enemyRand();
+          }
+          else {
+            playerHand = 'rock';
+            player.classList.add('rock');
+            battleActive = true;
+          }
+        }
+        break;
+      case 50: // Paper [ 2 ]
+        paper.classList.add('touch');
+        while (gameActive == true && battleActive == false) {
+          playerBox.classList.add('animated', 'rotateInDownLeft');
+          if (time == 0) {
+            playerHand = 'paper';
+            player.classList.add('paper');
+            battleActive = true;
+            enemyRand();
+          }
+          else {
+            playerHand = 'paper';
+            player.classList.add('paper');
+            battleActive = true;
+          }
+        }
+        break;
+      case 51: // Scissors [ 3 ]
+        scissors.classList.add('touch');
+        while (gameActive == true && battleActive == false) {
+          playerBox.classList.add('animated', 'rotateInDownLeft');
+          if (time == 0) {
+            playerHand = 'scissors';
+            player.classList.add('scissors');
+            battleActive = true;
+            enemyRand();
+          }
+          else {
+            playerHand = 'scissors';
+            player.classList.add('scissors');
+            battleActive = true;
+          }
+        }
+        break;
+      case 13: // New Game [ Enter ]
+        gameInit();
+        break;
+      case 27: // Main Menu [ Esc ]
+        // Go to Menu?
+        // Resume?
+        // Exit?
+        // show modal window
+        break;
     }
   });
-});
-
-// Rock [ 1 ]
-document.addEventListener('keydown', function(e) {
-  if (e.keyCode == 49) {
-    while (gameActive == true && battleActive == false) {
-      if (time == 0) {
-        playerHand = 'rock';
-        player.classList.add('rock');
-        battleActive = true;
-        enemyRand();
-      }
-      else {
-        playerHand = 'rock';
-        player.classList.add('rock');
-        battleActive = true;
-      }
+  document.addEventListener('keyup', function(e) {  
+    switch (e.keyCode) {
+      case 49: // Rock [ 1 ]
+        rock.classList.remove('touch');
+        break;
+      case 50: // Paper [ 2 ]
+        paper.classList.remove('touch');
+        break;
+      case 51: // Scissors [ 3 ]
+        scissors.classList.remove('touch');
+        break;
     }
-  }
-});
+  });
 
-// Paper [ 2 ]
-document.addEventListener('keydown', function(e) {
-  if (e.keyCode == 50) {
-    while (gameActive == true && battleActive == false) {
-      if (time == 0) {
-        playerHand = 'paper';
-        player.classList.add('paper');
-        battleActive = true;
-        enemyRand();
-      }
-      else {
-        playerHand = 'paper';
-        player.classList.add('paper');
-        battleActive = true;
-      }
-    }
-  }
-});
+  // New Game
+  var newGame = document.getElementById('new');
 
-// Scissors [ 3 ]
-document.addEventListener('keydown', function(e) {
-  if (e.keyCode == 51) {
-    while (gameActive == true && battleActive == false) {
-      if (time == 0) {
-        playerHand = 'scissors';
-        player.classList.add('scissors');
-        battleActive = true;
-        enemyRand();
-      }
-      else {
-        playerHand = 'scissors';
-        player.classList.add('scissors');
-        battleActive = true;
-      }
-    }
-  }
-});
+  newGame.addEventListener('click', function() {
+    gameInit();
+  });
 
-
-
-// New Game
-var newGame = document.getElementById('new');
-newGame.addEventListener('click', function() {
-  gameReset();
-});
-
-document.addEventListener('keydown', function(e) {
-  if (e.keyCode == 13) {
-    gameReset();
-  }
-});
-
-
-
-
-
-// Game Reset
-function gameReset() {
-  
-  
-  while (countActive == false) {
-    ex.forEach(function(el) {
-      el.classList.remove('show');
-    });
-
-
-    gameActive = true;
-    playerHand = undefined;
-    enemyHand = undefined;
-    player.removeAttribute('class');
-    enemy.removeAttribute('class');
-    battleActive = false;
-    time = true;
-    count();
-  }
-
-}
-
-
-
-// Tie Breaker
-function tieBreaker() {
-  var tieTime = 0;
-  var tieCounter = setInterval(function() {
-    tieTime++
-    if (tieTime == 1) {
-      clearInterval(tieCounter);
-      tieTime = 0;
-      gameReset();
-    }
-  }, 1000);
-}
-
-
-
-// touch for keydown
-
-
-// timer no initiate after first
-
-
-// ESC == 27 to get to menu area
-
-
-
-}; // window end
+};
